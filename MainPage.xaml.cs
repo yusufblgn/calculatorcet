4daf060c2a21ad71327f99372062898e80a2a479
yusufblgn <76652219+yusufblgn@users.noreply.github.com>
2024-12-03 12:59:53 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿namespace CalculatorCet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        double FirstNumber;
+        bool isFirstNumberAfterOperator = true;
+        Operator PreviousOperator = Operator.None;
+
+        private void SubtractButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Subtract;
+        }
+
+        private void MultiplyButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Multiply;
+        }
+
+        private void DivideButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Divide;
+        }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Add;
+        }
+
+        private void OndalikButton_Clicked(object sender, EventArgs e)
+        {
+            if (!Display.Text.Contains("."))
+            {
+                Display.Text += ".";
+            }
+        }
+
+
+        void DoCalculation()
+        {
+            switch (PreviousOperator)
+            {
+                case Operator.None:
+                    FirstNumber = Double.Parse(Display.Text);
+                    break;
+                case Operator.Add:
+                    FirstNumber = FirstNumber + Double.Parse(Display.Text);
+                    break;
+                case Operator.Subtract:
+                    FirstNumber = FirstNumber - Double.Parse(Display.Text);
+                    break;
+                case Operator.Multiply:
+                    FirstNumber = FirstNumber * Double.Parse(Display.Text);
+                    break;
+                case Operator.Divide:
+                    FirstNumber = FirstNumber / Double.Parse(Display.Text);
+                    break;
+            }
+            isFirstNumberAfterOperator = true;
+            Display.Text = FirstNumber.ToString();
+        }
+
+        private void Digit_Clicked(object sender, EventArgs e)
+        {
+            Button digitButton = sender as Button;
+            if (isFirstNumberAfterOperator)
+            {
+                Display.Text = digitButton.Text;
+                isFirstNumberAfterOperator = false;
+            }
+            else
+            {
+                Display.Text += digitButton.Text;
+            }
+        }
+
+        private void EqualButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.None;
+        }
+
+        private void CEButton_Clicked(object sender, EventArgs e)
+        {
+            Display.Text = "0";
+            isFirstNumberAfterOperator = true;
+        }
+
+        private void CButton_Clicked(object sender, EventArgs e)
+        {
+            Display.Text = "0";
+            FirstNumber = 0;
+            PreviousOperator = Operator.None;
+            isFirstNumberAfterOperator = true;
+        }
+
+        // Yeni: Virgül (ondalık ayırıcı) butonu
+        private void CommaButton_Clicked(object sender, EventArgs e)
+        {
+            if (!Display.Text.Contains("."))
+            {
+                Display.Text += ".";
+            }
+        }
+    }
+}
